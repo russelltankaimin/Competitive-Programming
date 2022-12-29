36e19ca378e49b8bab31cf3d942b88972e444260
Tan Kai Min Russell <russelltanreg38@gmail.com>
2022-12-29 23:18:38 +0800

    Some easy problems

--- codelib/teque.hpp
+++ codelib/teque.hpp
@@ -13,64 +13,6 @@ typedef vector<vector<vector<int>>> vv3d;
 #define pf push_front
 #define FOR(init, exc,itr) for(int i = init; i < exc; i+=itr)
 
-void printVector(vector<int> a) {
-  for (int i = 0; i < a.size(); i++) {
-    cout << a[i] << " ";
-  }
-  cout << endl;
-}
-
-template<typename T>
-void printMatrix(vector<vector<T>> matrix){
-  for (int i = 0; i<matrix.size();i++){
-    for (int j = 0;j<matrix[i].size();j++){
-      cout<<matrix[i][j]<<" ";
-    }
-    cout<<endl;
-  }
-
-}
-void printTensor(vv3d tensor){
-  for (int i = 0; i<tensor.size();i++){
-    for (int j = 0;j<tensor[i].size();j++){
-      for (int k =0; k<tensor[i][j].size();k++){
-        cout<<tensor[i][j][k]<<" ";
-      }
-      cout<<"  "<<endl;
-    }
-    cout<<"    "<<endl;
-  }
-}
-
-template<typename T, typename U, typename V>
-T min(U a, V b){
-  return a < b ? a : b;
-}
-template<typename T, typename U, typename V>
-T max(U a, V b){
-  return a > b ? a : b;
-}
-template<typename T,typename U>
-void debug(pair<T,U> a, bool submit){
-  if(!submit){
-    cout <<"[ ";
-  }
-  for(int i = 0; i < a.size();i++){
-    cout << "(" << a[i].first << "," << a[i].second <<")" << " ";
-  }
-  cout <<(!submit ? " ]" : "")<< endl;
-}
-template<typename T>
-void debug(T a, bool submit){
-  if(!submit){
-    cout <<"[ ";
-  }
-  for(int i = 0; i < a.size();i++){
-    cout << a[i] << " ";
-  }
-  cout <<(!submit ? " ]" : "")<< endl;
-}
-
 template<typename T>
 struct teque {
   deque<T> front;
@@ -157,27 +99,3 @@ struct teque {
   }
 };
 
-void solve() {
-  teque<long long> t = teque<long long>();
-  long long N; cin >> N; 
-  while(N--) {
-    string cmd; long long i;
-    cin >> cmd >> i;
-    if (cmd.compare("push_back") == 0) {
-      t.pushback(i);
-    } else if (cmd.compare("push_front") == 0) {
-      t.pushfront(i);
-    } else if (cmd.compare("push_middle") == 0) {
-      t.pushmiddle(i);
-    } else {
-      cout << t.get(i) << endl;
-    }
-  }
-}
-
-int main(){
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  solve();
-  return 0;
-}
