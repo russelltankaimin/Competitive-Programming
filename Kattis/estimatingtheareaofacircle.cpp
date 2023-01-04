5623b6063d62cc49b3a560c24156239c11c21ea2
Tan Kai Min Russell <russelltanreg38@gmail.com>
2023-01-04 17:32:02 +0800

    Easy Kattis Problems Test

---
+++ Kattis/estimatingtheareaofacircle.cpp
@@ -0,0 +1,108 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+typedef long long ll;
+typedef bitset<22> BITS;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<vector<vector<int>>> vv3d;
+#define ALL(x) x.begin(),x.end()
+#define UNIQUE(x) (x).resize(unique(ALL(x)) - x.begin())
+#define pb push_back
+#define pf push_front
+#define FOR(init, exc,itr) for(int i = init; i < exc; i+=itr)
+
+template<typename T>
+void printVector(vector<T> a) {
+  for (int i = 0; i < a.size(); i++) {
+    cout << a[i] << " ";
+  }
+  cout << endl;
+}
+
+template<typename T, typename S>
+void printVector(vector<pair<T,S>> a){
+  for (int i = 0; i < a.size(); i++) {
+    cout << "( " << a[i].first << ", " << a[i].second << " ) ";
+  }
+  cout << endl;
+}
+
+template<typename T>
+void printMatrix(vector<vector<T>> matrix){
+  for (int i = 0; i<matrix.size();i++){
+    for (int j = 0;j<matrix[i].size();j++){
+      cout<<matrix[i][j]<<" ";
+    }
+    cout<<endl;
+  }
+
+}
+
+template<typename T, typename S>
+void printMatrix(vector<vector<pair<T,S>>> matrix){
+  for (int i = 0; i<matrix.size();i++){
+    printVector<T,S>(matrix[i]);
+  }
+
+}
+
+void printTensor(vv3d tensor){
+  for (int i = 0; i<tensor.size();i++){
+    for (int j = 0;j<tensor[i].size();j++){
+      for (int k =0; k<tensor[i][j].size();k++){
+        cout<<tensor[i][j][k]<<" ";
+      }
+      cout<<"  "<<endl;
+    }
+    cout<<"    "<<endl;
+  }
+}
+
+template<typename T, typename U, typename V>
+T min(U a, V b){
+  return a < b ? a : b;
+}
+template<typename T, typename U, typename V>
+T max(U a, V b){
+  return a > b ? a : b;
+}
+template<typename T,typename U>
+void debug(pair<T,U> a, bool submit){
+  if(!submit){
+    cout <<"[ ";
+  }
+  for(int i = 0; i < a.size();i++){
+    cout << "(" << a[i].first << "," << a[i].second <<")" << " ";
+  }
+  cout <<(!submit ? " ]" : "")<< endl;
+}
+template<typename T>
+void debug(T a, bool submit){
+  if(!submit){
+    cout <<"[ ";
+  }
+  for(int i = 0; i < a.size();i++){
+    cout << a[i] << " ";
+  }
+  cout <<(!submit ? " ]" : "")<< endl;
+}
+
+void solve() {
+  float r,m,c;
+  float PI = 3.1415926539;
+  cin >> r >> m >> c;
+  while(r != 0 && m != 0 && c != 0) {
+    float ra = PI * r * r;
+    float rb = 4.0 * c/m * r * r;
+    cout << setprecision(12) << ra << " " << rb <<endl;
+    cin >> r >> m >> c;
+  }
+}
+
+int main(){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  solve();
+  return 0;
+}
