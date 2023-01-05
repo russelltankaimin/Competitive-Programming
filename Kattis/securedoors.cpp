33809e04dd8dfafd963baef4224a2a0dfcfe4797
Tan Kai Min Russell <russelltanreg38@gmail.com>
2023-01-05 22:45:47 +0800

    level 1.8 problems cleared, less securedoors

---
+++ Kattis/securedoors.cpp
@@ -0,0 +1,121 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+typedef long long ll;
+typedef bitset<22> BITS;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<vector<vector<int>>> vv3d;
+#define ALL(x) x.begin(),x.end()
+#define UNIQUE(x) (x).resize(unique(ALL(x)) - x.begin())
+#define pb push_back
+#define pf push_front
+#define FOR(init, exc,itr) for(int i = init; i < exc; i+=itr)
+
+template<typename T>
+void printVector(vector<T> a) {
+  for (int i = 0; i < a.size(); i++) {
+    cout << a[i] << " ";
+  }
+  cout << endl;
+}
+
+template<typename T, typename S>
+void printVector(vector<pair<T,S>> a){
+  for (int i = 0; i < a.size(); i++) {
+    cout << "( " << a[i].first << ", " << a[i].second << " ) ";
+  }
+  cout << endl;
+}
+
+template<typename T>
+void printMatrix(vector<vector<T>> matrix){
+  for (int i = 0; i<matrix.size();i++){
+    for (int j = 0;j<matrix[i].size();j++){
+      cout<<matrix[i][j]<<" ";
+    }
+    cout<<endl;
+  }
+
+}
+
+template<typename T, typename S>
+void printMatrix(vector<vector<pair<T,S>>> matrix){
+  for (int i = 0; i<matrix.size();i++){
+    printVector<T,S>(matrix[i]);
+  }
+
+}
+
+void printTensor(vv3d tensor){
+  for (int i = 0; i<tensor.size();i++){
+    for (int j = 0;j<tensor[i].size();j++){
+      for (int k =0; k<tensor[i][j].size();k++){
+        cout<<tensor[i][j][k]<<" ";
+      }
+      cout<<"  "<<endl;
+    }
+    cout<<"    "<<endl;
+  }
+}
+
+template<typename T, typename U, typename V>
+T min(U a, V b){
+  return a < b ? a : b;
+}
+template<typename T, typename U, typename V>
+T max(U a, V b){
+  return a > b ? a : b;
+}
+template<typename T,typename U>
+void debug(pair<T,U> a, bool submit){
+  if(!submit){
+    cout <<"[ ";
+  }
+  for(int i = 0; i < a.size();i++){
+    cout << "(" << a[i].first << "," << a[i].second <<")" << " ";
+  }
+  cout <<(!submit ? " ]" : "")<< endl;
+}
+template<typename T>
+void debug(T a, bool submit){
+  if(!submit){
+    cout <<"[ ";
+  }
+  for(int i = 0; i < a.size();i++){
+    cout << a[i] << " ";
+  }
+  cout <<(!submit ? " ]" : "")<< endl;
+}
+
+void solve() {
+  int N; cin >> N;
+  unordered_map<string, bool> inside;
+  while(N--) {
+    string action, name; cin >> action >> name;
+    if (action.compare("entry") == 0) {
+      if (inside.count(name) != 0 || inside[name]) {
+        inside[name] = true;
+        printf("%s entered (ANOMALY)\n", name.c_str());
+      } else {
+        inside[name] = true;
+        printf("%s entered\n", name.c_str());
+      }
+    } else {
+      if (inside.count(name) == 0 || !inside[name]) {
+        inside[name] = false;
+        printf("%s exited (ANOMALY)\n", name.c_str());
+      } else {
+        inside[name] = false;
+        printf("%s exited\n", name.c_str());
+      }
+    }
+  }
+}
+
+int main(){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  solve();
+  return 0;
+}
